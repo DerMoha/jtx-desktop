ae00fa4d3f276e6febbfc7b65219b87411a41b25
DerMoha <46383545+DerMoha@users.noreply.github.com>
2026-04-22 22:47:16 +0200

    chore: add build files and gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,18 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    kotlin("multiplatform") version "2.1.0"
+    kotlin("multiplatform") version "2.2.0"
     id("org.jetbrains.compose") version "1.7.3" 
-    kotlin("plugin.serialization") version "2.1.0"
-    kotlin("plugin.compose") version "2.1.0"
+    kotlin("plugin.serialization") version "2.2.0"
+    kotlin("plugin.compose") version "2.2.0"
 }
 
 group = "com.jtx.desktop"
-version = "0.1"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
+    google()
 }
 
 kotlin {
@@ -23,17 +24,17 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                api("androidx.compose.ui:ui:1.7.3")
-                api("androidx.compose.foundation:foundation:1.7.3")
-                api("androidx.compose.material3:material3:1.3.2")
-                api("androidx.compose.components:components-resources:1.7.3")
+                api(compose.ui)
+                api(compose.foundation)
+                api(compose.material3)
+                api(compose.components.resources)
                 api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
                 api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
             }
         }
         val jvmMain by getting {
             dependencies {
-                implementation("androidx.compose.ui:ui-desktop:1.7.3")
+                implementation(compose.desktop.common)
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
                 implementation("com.google.code.gson:gson:2.11.0")
             }
