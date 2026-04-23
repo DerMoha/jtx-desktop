b1c3b8a0d8ddf8b83f98703047639b0584eb06df
DerMoha <46383545+DerMoha@users.noreply.github.com>
2026-04-23 08:40:02 +0200

    fix: Skiko native lib loading for macOS ARM64

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     kotlin("multiplatform") version "2.2.0"
-    id("org.jetbrains.compose") version "1.7.3" 
+    id("org.jetbrains.compose") version "1.7.3"
     kotlin("plugin.serialization") version "2.2.0"
     kotlin("plugin.compose") version "2.2.0"
 }
@@ -13,12 +13,14 @@ version = "1.0.0"
 repositories {
     mavenCentral()
     google()
+    maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
+    maven("https://cache-redirector.jetbrains.com/packages.jetbrains.team/maven/p/ij/intellij-dependencies")
 }
 
 kotlin {
     jvm {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
+            jvmTarget.set(JvmTarget.JVM_21)
         }
     }
     sourceSets {
@@ -30,6 +32,7 @@ kotlin {
                 api(compose.components.resources)
                 api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
                 api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+                
             }
         }
         val jvmMain by getting {
@@ -37,6 +40,7 @@ kotlin {
                 implementation(compose.desktop.common)
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
                 implementation("com.google.code.gson:gson:2.11.0")
+                implementation("org.jetbrains.skiko:skiko:0.9.18")
             }
         }
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,8 @@ dependencyResolutionManagement {
     repositories {
         mavenCentral()
         google()
+        maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
+        maven("https://cache-redirector.jetbrains.com/packages.jetbrains.team/maven/p/ij/intellij-dependencies")
     }
 }
 
